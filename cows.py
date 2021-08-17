d22b3fcfdd823405091aad26b787c5abea3b9af9
Pankarinko <panka@panka.localdomain>
2021-08-17 17:19:12 +0200

    Add layers

--- cows.py
+++ cows.py
@@ -1,2 +1,13 @@
 import tensorflow
 from tensorflow import keras
+from tensorflow.keras import layers
+
+
+model = keras.Sequential(
+    [
+        layers.Dense(5, activation="relu", name="l1"),
+        layers.Dense(12, name="l2"),
+    ]
+    )
+
+model.add(Activation("softmax"))
