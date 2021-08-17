c5e748c2c57d33338c9c980dafba978a6e02b1b8
Pankarinko <pankarinko@gmail.com>
2021-08-17 19:49:33 +0200

    Define training parameters

--- cows.py
+++ cows.py
@@ -1,6 +1,9 @@
-import tensorflow
-from tensorflow import keras
+import tensorflow as tf
+import tensorflow.keras as keras
 from tensorflow.keras import layers
+import numpy as np
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
 
 
 model = keras.Sequential(
@@ -8,6 +11,11 @@ model = keras.Sequential(
         layers.Dense(5, activation="relu", name="l1"),
         layers.Dense(12, name="l2"),
     ]
-    )
+ )
 
-model.add(Activation("softmax"))
+model.add(layers.Activation("softmax"))
+model.compile(Adam(learning_rate=.0001), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_samples, train_labels, batch="10", epochs="20", shuffle=True, verbose=2)
+
+x = tf.ones((3, 3))
+y = model(x)
