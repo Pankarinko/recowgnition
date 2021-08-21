81c9177719e42c43fae3180bfbb3f2a54b768f87
Pankarinko <pankarinko@gmail.com>
2021-08-21 03:32:31 +0200

    Generate random training samples

--- recowgnition.py
+++ recowgnition.py
@@ -13,9 +13,29 @@ model = keras.Sequential(
     ]
  )
 
+train_samples = []
+train_labels = []
+
+for i in range(50)
+    young = randint(13, 64)
+    train_samples.append(young)
+    train_labels.append(1)
+    old = randint(65, 100)
+    train_samples.append(old)
+    train_labels.append(0)
+
+for i in range(1000)
+    young = randint(13, 64)
+    train_samples.append(young)
+    train_labels.append(0)
+    old = randint(65, 100)
+    train_samples.append(old)
+    train_labels.append(1)
+
 model.add(layers.Activation("softmax"))
 model.compile(Adam(learning_rate=.0001), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 model.fit(train_samples, train_labels, batch="10", epochs="20", shuffle=True, verbose=2)
+
 
 x = tf.ones((3, 3))
 y = model(x)
